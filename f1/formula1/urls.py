ef9896d25c9453d809454925a62bd6d515040581
Ilya <ilyamironov2102@gmail.com>
2023-12-24 14:39:46 +0300

    added functions for delete and update objects

--- f1/formula1/urls.py
+++ f1/formula1/urls.py
@@ -13,7 +13,11 @@ path('signup/', views.signup_view, name='signup'),
 path('race-results/<int:race_id>/', views.race_results_view, name='race_results'),
 path('drivers-info/', views.drivers_info, name='drivers_info'),
 path('driver-info/<int:driver_id>/', views.driver_info, name='driver_info'),
+path('add_driver/', views.add_driver, name='add_driver'),
 path('teams-info/', views.teams_info, name='teams_info'),
 path('team-info/<int:team_id>/', views.team_info_view, name='team_info'),
+path('add_team/', views.add_team, name='add_team'),
+path('delete_team/<int:team_id>/', views.delete_team, name='delete_team'),
+path('edit_team/<int:team_id>/', views.edit_team, name='edit_team'),
 path('', views.home, name='home'),  # Home page
 ]
--- f1/formula1/views.py
+++ f1/formula1/views.py
@@ -41,13 +41,39 @@ def race_results_view(request, race_id):
 
 def drivers_info(request):
     drivers = Driver.objects.all()  # Получение всех гонщиков
-    return render(request, 'drivers_info.html', {'drivers': drivers})
+    teams = Team.objects.all()  # Получение всех команд
+    return render(request, 'drivers_info.html', {'drivers': drivers, 'teams': teams})
 
 
 def driver_info(request, driver_id):
     driver = get_object_or_404(Driver, pk=driver_id)
     return render(request, 'driver_info.html', {'driver': driver})
+def add_driver(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        abbreviation = request.POST.get('abbreviation')
+        team_id = request.POST.get('team')
+        grand_slam = request.POST.get('grand_slam')
+        fastest_lap_count = request.POST.get('fastest_lap_count')
+        races_count = request.POST.get('races_count')
+        wins_count = request.POST.get('wins_count')
 
+        team = Team.objects.get(id=team_id)
+
+        new_driver = Driver(
+            name=name,
+            abbreviation=abbreviation,
+            team=team,
+            grand_slam=int(grand_slam),
+            fastest_lap_count=int(fastest_lap_count),
+            races_count=int(races_count),
+            wins_count=int(wins_count),
+        )
+
+        new_driver.save()
+        drivers = Driver.objects.all()  # Получение всех гонщиков
+        teams = Team.objects.all()  # Получение всех команд
+        return redirect('drivers_info') 
 
 def team_info_view(request, team_id):
     team = get_object_or_404(Team,  pk=team_id)
@@ -56,3 +82,46 @@ def team_info_view(request, team_id):
 def teams_info(request):
     teams = Team.objects.all()  # Получение всех гонщиков
     return render(request, 'teams_info.html', {'teams': teams})
+
+def add_team(request):
+    if request.method == "POST":
+        team_name = request.POST.get('team_name')
+        team_wins = request.POST.get('team_wins')
+        team_races = request.POST.get('team_races')
+        description = request.POST.get('description')
+
+        new_team = Team(
+            team_name=team_name,
+            team_wins=team_wins,
+            team_races=team_races,
+            description=description
+        )
+        
+        new_team.save()
+
+        return redirect('teams_info') 
+    
+
+def delete_team(request, team_id):
+    if request.method == "POST":
+        team = Team.objects.get(id=team_id)
+        team.delete()
+        return redirect('teams_info') # Redirect to the teams list page
+
+    # Redirect or show an error if not a POST request
+    return redirect('teams_info')
+
+
+def edit_team(request, team_id):
+    team = get_object_or_404(Team, id=team_id)
+
+    if request.method == "POST":
+        # Update team with new data from form
+        team.team_name = request.POST.get('team_name')
+        team.team_wins = request.POST.get('team_wins')
+        team.team_races = request.POST.get('team_races')
+        team.description = request.POST.get('description')
+        team.save()
+
+        # Redirect to team info or list
+        return redirect('teams_info')
